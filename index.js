92fb90109cabfa49cb327f2556f332be154a49f5
tamaina <tamaina@hotmail.co.jp>
2023-02-06 14:52:47 +0000

    :v:

--- index.js
+++ index.js
@@ -3,5 +3,7 @@
 import awsLambdaFastify from '@fastify/aws-lambda';
 import { init } from './app.js';
 
+const app = init();
 const proxy = awsLambdaFastify(init());
 export const handler = proxy;
+await app.ready(); // https://github.com/fastify/aws-lambda-fastify#lower-cold-start-latency
