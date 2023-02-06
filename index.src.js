794eef286ab21b38782ecd9ca27125eae1d3024a
tamaina <tamaina@hotmail.co.jp>
2023-02-06 15:32:55 +0000

    Revert "esbuild test"
    This reverts commit abb36a6e176853a69ad7ffeae68a8d1997c2bb15.

--- index.src.js
+++
@@ -1,9 +0,0 @@
-// https://www.fastify.io/docs/latest/Guides/Serverless/#lambdajs
-
-import awsLambdaFastify from '@fastify/aws-lambda';
-import { init } from './app.js';
-
-const app = init();
-const proxy = awsLambdaFastify(init());
-export const handler = proxy;
-await app.ready(); // https://github.com/fastify/aws-lambda-fastify#lower-cold-start-latency
